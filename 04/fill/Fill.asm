be0ae97558a231d45a11010c638ce2189cb21da1
rossen1020 <99935090+rossen1020@users.noreply.github.com>
2023-01-06 20:50:19 +0800

    Add files via upload

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,44 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+//參照老師在教學時給看的程式碼
+
+(FOREVER)
+@16384
+D=A
+@i
+M=D
+
+@24576
+D=A
+
+@i
+D=M-D
+@WEND
+D;JGE
+
+@COLOR
+M=0
+@24576
+D=M 
+@NEXT
+D;JEQ
+
+@COLOR
+M=-1
+
+(NEXT)
+@COLOR
+D=M
+@i
+A=M
+M=D
+
+@i
+M=M+1
+@WHILE
+0;JMP
+
+(WEND)
+@FOREVER
+0;JMP
